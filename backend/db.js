5ea4deb71bdc86078e90d46bfc5dad1eff7a27c6
akrem.zayane <akremzayane@gmail.com>
2023-09-28 12:44:31 +0200

    updated

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 //connect to db 
 function connectDB() {
-    mongoose.connect('mongodb+srv://akrammongo:mongo123@cluster0.z8ey1jj.mongodb.net/GateCars')
+    mongoose.connect(process.env.MONGODB)
     const connection = mongoose.connection
     connection.on('connected', () => {
         console.log('Mongo DB connection Succesfully')
